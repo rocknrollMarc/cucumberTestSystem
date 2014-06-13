23e16e4ee6229e60a1ff76ffcb94c2c226fe877a
Marc Blümner <m.bluemner@liquidlabs.de>
2014-06-13 19:29:49 +0200

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CucumberTestSystem::Application.config.secret_token = 'eb15331b78d3d33f7572729cb0a0db796e319df22678a9fd2c5b525f4cc3df713b47d01945766cdf02c1c4ba331aa100a3760b74831ca0a06bec533ac5556342'
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_cucumberTestSystem_session'
+CucumberTestSystem::Application.config.session_store :cookie_store, key: '_cucumberTestSystem_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# CucumberTestSystem::Application.config.session_store :active_record_store
