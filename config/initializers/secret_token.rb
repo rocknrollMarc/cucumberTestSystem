3f02033edecfc4446339903e26e535d05e5b4246
Marc Blümner <m.bluemner@liquidlabs.de>
2014-06-13 19:31:31 +0200

    rails_apps_composer: initial commit

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-CucumberTestSystem::Application.config.secret_token = 'c96e3b408726a2d0520e3b4abac1008fc8378e673ed6f387f7547a69b34b86ae9732addc0be8f3fea88a044b59fa237d4727212093562235cc5a146ac2345cd9'
+CucumberTestSystem::Application.config.secret_token = '157a1e1e2bd121e4b637ff11cfd61915df50c93583d3ce642ed2b1588e2753fc9b6b597983a5493e6f92bf3ea10eb7eea79e0df1c9f13e1c834ef3bd22be0e2d'
