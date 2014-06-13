a7eab0503a56ff78b6e6d0788e4d345330a126cd
Marc Blümner <m.bluemner@liquidlabs.de>
2014-06-13 19:30:49 +0200

    rails_apps_composer: initial commit

--- Gemfile
+++ Gemfile
@@ -5,7 +5,6 @@ gem 'rails', '3.1.3'
 # Bundle edge Rails instead:
 # gem 'rails',     :git => 'git://github.com/rails/rails.git'
 
-gem 'sqlite3'
 
 
 # Gems used only for assets and not required
@@ -30,7 +29,3 @@ gem 'jquery-rails'
 # To use debugger
 # gem 'ruby-debug19', :require => 'ruby-debug'
 
-group :test do
-  # Pretty printed test output
-  gem 'turn', '~> 0.8.3', :require => false
-end
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-CucumberTestSystem::Application.config.secret_token = 'eb15331b78d3d33f7572729cb0a0db796e319df22678a9fd2c5b525f4cc3df713b47d01945766cdf02c1c4ba331aa100a3760b74831ca0a06bec533ac5556342'
+CucumberTestSystem::Application.config.secret_token = 'c96e3b408726a2d0520e3b4abac1008fc8378e673ed6f387f7547a69b34b86ae9732addc0be8f3fea88a044b59fa237d4727212093562235cc5a146ac2345cd9'
